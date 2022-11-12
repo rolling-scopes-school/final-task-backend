4010cf4c2f14f0e4a7fd89f1e7abcf35a81fb0b0
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-12 14:32:27 +0300

    feat: add own data Sat, 12 Nov 2022 14:32:27 +0300

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,18 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      'mongodb+srv://MatsveiDubaleka:m29092003O@project-management-app.uvhr9iu.mongodb.net/?retryWrites=true&w=majority'
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
