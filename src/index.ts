0791eb1b823f40ea2678bce892a39f575dd340ab
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-12 17:50:03 +0300

    fix: fix build

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,19 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+
 (async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://MatsveiDubaleka:m29092003O@project-management-app.uvhr9iu.mongodb.net/?retryWrites=true&w=majority'
-    );
+    await mongoose.connect('mongodb+srv://MatsveiDubaleka:m29092003O@project-management-app.uvhr9iu.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    });
+    })
   } catch (error) {
     console.log(error);
   }
 })();
+
+
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
