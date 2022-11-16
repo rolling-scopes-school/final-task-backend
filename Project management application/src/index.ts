56f20cb907e640c170c9eb2ae443b7b21b793b67
Stanislau Yarotski <stas_yar@tut.by>
2022-11-16 17:05:02 +0300

    feat: add dotenv to dependencies

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import { PORT } from './constants';
+import * as dotenv from 'dotenv';
 
 import * as serverService from './services/server.service';
 
+dotenv.config();
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`mongodb+srv://${process.env.DEV_USERNAME}:${process.env.PASSWORD}@${process.env.CLASTER_INFO}.mongodb.net/managerApp`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
