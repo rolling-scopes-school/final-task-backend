c913ac1d4b85660420bb97a429c8c905f062951b
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-17 13:05:45 +0300

    feat: add description to board and change titles of boards in swagger Thu, 17 Nov 2022 13:05:45 +0300

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,19 +1,26 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
-const boardScheme = new Schema({
-  title: {
-    type: String,
-    required: true,
+const boardScheme = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    owner: {
+      type: String,
+      required: true,
+    },
+    users: {
+      type: [String],
+      required: true,
+    },
   },
-  owner: {
-    type: String,
-    required: true,
-  },
-  users: {
-    type: [String],
-    required: true,
-  }
-}, { versionKey: false });
+  { versionKey: false }
+);
 
 export default mongoose.model('Board', boardScheme);
