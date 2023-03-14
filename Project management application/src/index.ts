7fb96a3f5f78432ca74838962c69d6573058ead2
HelioGonzales <heliogonzalesr@gmail.com>
2023-03-14 20:53:15 +0100

    start backend deploy

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      // "mongodb+srv://heliogonzalesr:Statusquo1004@managerappbackend.xbgwhd.mongodb.net/managerApp"
+      "mongodb+srv://heliogonzalesr:Statusquo1004@managerappbackend.xbgwhda.mongodb.net/?retryWrites=true&w=majority"
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
