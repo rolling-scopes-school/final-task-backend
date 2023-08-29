edc281e333ec3cfa152a4584507ba561dbaeb608
Nikita <snitkonikita@gmail.com>
2023-08-29 16:46:24 +0600

    feat: add new mongodb link

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://Admin:SPndCKkzGz4rq5z6@cluster0.mjyymgr.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
